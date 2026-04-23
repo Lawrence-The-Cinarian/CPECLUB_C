ef0e165ca371686cf4ce6d9d624000358b5035db
Lawrence-The-Cinarian <cinarilabs@gmail.com>
2026-04-23 20:56:30 +0100

    Adding Comments to the eoc.c file

--- Even-Odd-Checker/eoc.c
+++ Even-Odd-Checker/eoc.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
-#include <math.h>
+//eoc means Even-Odd-Chevker
+#include <stdio.h> //Standard Input Output Library
+#include <math.h>  //Mathematics Library
 
 
 int main() {
 
-	int num;
-	puts("This is the Even Odd Checker File\n");
-	printf("Enter Number to check: ");
+	int num; // The variable for num which represents "Input Number" int is just a dats type
+	puts("This is the Even Odd Checker File\n"); // Introduction file
+	printf("Enter Number to check: "); // Where
 	scanf("%d", &num);
+
+	/* Conditionals indicating if the number is divisible by 2 if so 
+	 * then it puts "The Number is Even"
+	 * if the number is not divisible by 2 is puts "The Number is Odd"
+	*/
 
 	if(num % 2 == 0) {
 	puts("The Number is Even");
  }
 	else puts("The Number is Odd");
-return 0;
+return 0; 
 }
