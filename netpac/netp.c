f6ac2854a056c4fe9abde912452c2e85c4e4fc97
Lawrence-The-Cinarian <cinarilabs@gmail.com>
2026-04-27 15:51:34 +0100

    Commit for My Networking Project

---
+++ netpac/netp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/socket.h>
+/*
+ * I was tasked to create a Network Packet Filter which willbe used in *
+ * making Serial Numbers with lower numbering the main priority to be  *
+ * executed first.						       *
+ * For Example; A packet Serial Number has 300 and the priority was 1, *
+ * B packet has has Serial Number has 75 ad the priority was 11, A pa- *
+ * cket will be processed first before the second one
+ * The Process I followed will be communicated in this comment
+ */
+
+	uint64_t main() {
+
+
+		printf("Welcome to Cinel's Network Packet Filter\n");
+		printf("")
+
+
+}
