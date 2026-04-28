96fb30e4a9fe7a7c931597b58b4ce139a7f1986e
Lawrence-The-Cinarian <cinarilabs@gmail.com>
2026-04-28 13:37:31 +0100

    Syill updating and fixing bugs

--- netpac/netp.c
+++ netpac/netp.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/socket.h>
 /*
- * I was tasked to create a Network Packet Filter which willbe used in *
- * making Serial Numbers with lower numbering the main priority to be  *
- * executed first.						       *
- * For Example; A packet Serial Number has 300 and the priority was 1, *
- * B packet has has Serial Number has 75 ad the priority was 11, A pa- *
- * cket will be processed first before the second one
+ * I was tasked to create a Network Packet Filter (Simulated) which will 
+ * be used in making Serial Numbers with lower numbering the main priority to be  
+ * executed first.						       
+ * For Example; A packet Serial Number has 300 and the priority was 1, 
+ * B packet has has Serial Number has 75 ad the priority was 11, A packet 
+ * will be processed first before the second one
  * The Process I followed will be communicated in this comment
  */
 
-	uint64_t main() {
+int main(void) {
+      int serialNumber[10];
+      int Priority[10];
+      char Packet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+      
+      puts("Cinari Firewall");
+      puts("Instructions; You are to enter The Priority and Serial Numbers of your Packets for sorting and arrangement");
+      puts("");
+      for(int ps = 0; ps <= 10; ps++) {
+       printf("Enter Priority Number for Packet %c: ", Packet[ps]);
+       scanf("%d", &Priority[ps]);
+       printf("Enter Serial Number for Packet %c: ", Packet[ps]);
+       scanf("%d", &serialNumber[ps]);
+       puts("");
+      }
+      
 
-
-		printf("Welcome to Cinel's Network Packet Filter\n");
-		printf("")
-
-
+return 0;
 }
