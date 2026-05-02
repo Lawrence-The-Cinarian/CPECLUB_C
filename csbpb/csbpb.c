ef6ba5c2bcaa61e840f6b3e9e3ee3567009309c1
Lawrence-The-Cinarian <cinarilabs@gmail.com>
2026-05-02 06:41:55 +0100

    Making Some Updates and adding a new folder

---
+++ csbpb/csbpb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "custom.h"
+
+
+int main() {
+
+	Contact savemy[2];
+	int count = 0;
+	char *ptr = &savemy[count].Address;
+	int number;
+
+for(;;) {
+	printf("Enter 1 for Saving Contacts, Enter 2 for viewing saved contacts\n");
+	printf("Enter Number: ");
+	scanf("%d", &number);
+
+		if(number == 1) {
+	for(int i = 0; i < count; i++) {
+	printf("Enter your Name: ");
+	scanf("%99s", savemy[count].Name);
+	printf("Entet your Phone Number: ");
+	scanf("%99s", savemy[count].phoneNumber);
+
+
+	printf("Name: %s | Phone Number: %s | Memory Location %p |\n", savemy[count].Name, savemy[count].phoneNumber, &savemy[count].Address);
+ 	}
+}
+	else if(number == 2) {
+	for(int i = 0; i < count; i++) {
+	     printf("Name: %s | Phone Number: %s | Memory Location %p |\n", savemy[count].Name, savemy[count].phoneNumber, &savemy[count].Address);
+	}
+}
+	else puts("invalid option");
+	}
+
+return 0;
+}
---
+++ csbpb/custom.h
@@ -0,0 +1,13 @@
+#ifndef CUSTOM_H
+#define CUSTOM_H
+
+typedef struct {
+
+char phoneNumber[100];
+char Name[100];
+char Address;
+
+} Contact;
+
+
+#endif
