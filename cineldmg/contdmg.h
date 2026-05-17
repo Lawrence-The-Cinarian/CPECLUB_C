637a3966efa34ce669145de64e8c52d798d8a917
Lawrence-The-Cinarian <lawrencethecinarian@gmail.com>
2026-05-17 19:02:48 +0100

    Fixes and Number Settings

--- cineldmg/contdmg.h
+++ cineldmg/contdmg.h
@@ -5,12 +5,10 @@ int result(int a) {
 for(int i = 1; i <= a; i++) {
  for(int j = 1; j <= a; j++) {
  int mult = i * j;
- printf("Result: %d", mult);
-for(int k = 0; k < 4; k++) {
- printf("\n");
+ printf("%4d", mult);
    }
+  printf("\n");
   }
- }
 return 0;
 }
 #endif
